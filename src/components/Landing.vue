d6295e740f7378bf8dbbc2c4de89675c5933a19a
yassminee00 <yassmine00elgh@gmail.com>
2024-02-03 23:57:12 -0800

    Cleanup of unnecessary code

---
+++ src/components/Landing.vue
@@ -0,0 +1,407 @@
+<template>
+
+    <div class="landing-page">
+        <div class="on-mobile">
+            <div>&nbsp;</div>
+            <div>&nbsp;</div>
+            <div>&nbsp;</div>
+            <div class="mobile-table">
+                <h3>Check back soon for the mobile version!</h3>
+                <img class="no-mobile" src="./../../public/icons/sad_paw.png">
+            </div>
+        </div>
+        <div class="header-landing">
+            <img class="puppr-logo" src="./../../public/icons/logo.png">
+            <p :class="choice1" v-on:click="showIntro">
+                <img class="paws" src="./../../public/icons/matches.png">
+                Introduction</p>
+            <p :class="choice2" v-on:click="showFeatures">
+                <img class="paws" src="./../../public/icons/matches.png">
+                Features</p>
+            <p :class="choice3" v-on:click="showAbout">
+                <img class="paws" src="./../../public/icons/matches.png">
+                About</p>
+        </div>
+        <div class="upper-container">
+            <div class="info-pages">
+
+                <div
+                        class="intro"
+                        v-if="intro && !features && !about"
+                >
+                    <div class="intro-top">
+                        <h1>Welcome to PawfectMatch!</h1>
+                        <img class="my-logo" src="./../../public/icons/logo.png">
+                        <h3><i>Where Tail-Wagging Connections Begin!</i></h3>
+                        <br>
+                        <p>PawfectMatch is the best app on the internet to meet amazing pups and their owners!</p>
+                        <p>Browse through local dogs and connect with their owners to set up a playdate or a walk.</p>
+                        <p>If you like a dog and their owner likes your dog back, then you will match!</p>
+                    </div>
+                </div>
+                <div
+                        v-if="!intro && features && !about"
+                        class="features">
+                    <div class="row1">
+                        <img class="signin-ss" src="./../../public/icons/signin_screen.png">
+                        <div class="column1">
+                            <h3>Sign up</h3>
+                            <p>Make a profile that shows off your pup and their personality!<p>
+                            <p>Upload a picture to show the world.</p>
+                        </div>
+                    </div>
+                    <div class="row2">
+                        <div class="column2">
+                            <h3>Browse</h3>
+                            <p>Use a familiar and intuitive interface to see dogs in your area</p>
+                                <p>and express interest in connecting.</p>
+                        </div>
+                        <img class="browse-ss" src="./../../public/icons/browse_no_text.png">
+                    </div>
+                    <div class="row3">
+                            <img class="match-ss" src="./../../public/icons/match_screen.png">
+                        <div class="column3">
+                            <h3>Matches</h3>
+                            <p>Keep track of your matches in one convenient location.</p>
+                            <p>Display the contact email for matched dogs and make plans to connect.</p>
+                        </div>
+                    </div>
+                </div>
+                <div
+                        v-if="!intro && !features && about"
+                        class="about">
+                    <h3>Why PawfectMatch?</h3>
+                    <p>This is my Portfolio Project, concluding my Foundations Year at <a href="https://www.holbertonschool.com/">Holberton School</a>.</p>
+                    <p>The idea of PawfectMatch came from my passion and my love for dogs.
+                        PawfectMatch is not just a project; it's a labor of love, and I'm excited to share it with fellow dog enthusiasts.
+                        I know that dogs need to socialize as much as humans and we wanted to create a fun and intuitive interface
+                        to connect dog owners.</p>
+                    <br>
+                    <h4>The Team</h4>
+                    <p>Yassmine El Ghalbzouri - <a href="https://www.github.com/yassminee00">GitHub</a>
+                    <br>
+                    <h4>Check out the source code for PawfectMatch!</h4>
+                    <a href="https://github.com/yassminee00/pawfectmatch">pawfectmatch on GitHub</a>
+                </div>
+            </div>
+        </div>
+        <router-link class="button-container" :to="{ name: 'login' }">
+            <img class="login-button" src="./../../public/icons/login_button.png">
+        </router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                intro: true,
+                features: false,
+                about: false
+            };
+        },
+        methods: {
+            showIntro() {
+                this.intro = true;
+                this.features = false;
+                this.about = false;
+            },
+            showFeatures() {
+                this.intro = false;
+                this.features = true;
+                this.about = false;
+            },
+            showAbout() {
+                this.intro = false;
+                this.features = false;
+                this.about = true;
+            }
+        },
+        computed: {
+            choice1() {
+                if (this.intro) return "active";
+                else return "passive";
+            },
+            choice2() {
+                if (this.features) return "active";
+                else return "passive";
+            },
+            choice3() {
+                if (this.about) return "active";
+                else return "passive";
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    /* FOR BROWSER */
+    @media screen and (min-width: 821px) {
+
+        .on-mobile {
+            display: none;
+        }
+
+        * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+
+        .landing-page {
+            background-image: url('./../../public/icons/landing_page.png');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            font-family: "Arial Rounded MT Bold";
+            background-color: #F9EFEE;
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            right: 0;
+            left: 0;
+            margin: 0 auto !important;
+            overflow-x: hidden;
+            height: 100%;
+            width: 100%;
+        }
+
+        /* Content for header */
+        .header-landing {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+            width: 70%;
+            padding: 1em !important;
+            text-shadow: 1px 1px #6361613b;
+        }
+
+        .header-landing > p:hover {
+            color: #D58FDF;
+        }
+
+        .paws {
+            height: 37px;
+            width: 37px;
+        }
+
+        .pawfectmatch-logo {
+            height: 5em;
+        }
+
+        .intro p, .features p, .about p {
+            font-size: 20px;
+            color: #EAA8F4;
+        }
+
+        .intro h3, .intro h1, .features h3, .about h3, .about h4 {
+            color: #D58FDF;
+        }
+        .intro-top {
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+            width: 100%;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .my-logo {
+            height: 15em;
+            width: 22em;
+            margin: 1em 0 !important;
+        }
+
+        .intro-bot p {
+            font-size: 25px;
+            color: #EAA8F4;
+        }
+
+        .intro-bot img {
+            height: 12em !important;
+            margin-right: 1em !important;
+            margin-top: 1em !important;
+        }
+
+        .signin-ss {
+            height: 10em;
+        }
+
+        .match-ss {
+            height: 12em;
+            margin-top: -1em !important;
+        }
+        
+        .browse-ss {
+            height: 12em;
+            margin-top: -4em !important;
+            /* margin-right: 2em !important; */
+        }
+
+        .row1, .row2 {
+            width: 100%; 
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-start;
+        }
+
+        .row3 {
+            display: flex;
+            flex-direction: row;
+            width: 100%;
+            justify-content: flex-start;
+            margin-top: -2em !important;
+        }
+
+        .column1 {
+            display: flex;
+            flex-direction: column;
+            margin-left: 2em !important;
+            margin-left: 20em;
+            width: 100%;
+        }
+
+        .column2 {
+            display: flex;
+            flex-direction: column;
+            margin-top: -1em !important;
+            margin-right: 2em !important;
+            width: 100%;
+            text-align: right;
+        }
+
+        .column3 {
+            display: flex;
+            flex-direction:column;
+            margin-top: 2em !important;
+            margin-left: 2em !important;
+        }
+        
+        .upper-container {
+            width: 100%;
+            height: 70%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-top: 1px solid #EAA8F4;
+        }
+
+        /* Content from different tabs */
+        .info-pages {
+            flex-direction: row;
+            align-items: flex-start;
+            display: flex;
+            justify-content: space-evenly;
+            width: 90%;
+            height: 90%;
+            background-color: #fffffff0;
+            border-radius: 15px;
+            -webkit-box-shadow: 0px 0px 26px -6px rgba(0,0,0,0.45);
+            text-shadow: 1px 1px #6361613b;
+        }
+
+        .intro {
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+        }
+
+        .intro, .features, .about {
+            display: flex;
+            flex-direction: column;
+            align-items: baseline;
+            width: 100%;
+            padding: 1em !important;
+            animation: fadeIn 1s;
+        }
+        
+        @keyframes fadeIn {
+            0% {opacity: 0;}
+            100% {opacity: 1;}
+        }
+
+        .active {
+            font-size: 28px;
+            color: #f8a978;
+        }
+
+        .passive {
+            font-size: 26px;
+            color: #ffc5a1;
+        }
+
+        /* Button style */
+        .login-button {
+            opacity: 0.9;
+            height: 6em;
+            padding-right: 5% !important;
+        }
+
+        .login-button:hover {
+            opacity: 1;
+            height: 6em;
+            padding-right: 5% !important;
+        }
+
+        .button-container {
+            display: flex;
+            align-content: center;
+            justify-content: flex-end;
+        }
+    }
+
+    /* FOR MOBILE */
+    @media screen and (max-width: 820px) {
+
+
+        .header-landing, .upper-container, .login-button {
+            display: none;       
+        }
+
+        .on-mobile {
+            background-image: url('./../../public/icons/landing_page_mobile.png');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            font-family: "Arial Rounded MT Bold";
+            background-color: #F9EFEE;
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            right: 0;
+            left: 0;
+            margin: 0 auto;
+            overflow-x: hidden;
+            height: 100%;
+            width: 100%;
+        }
+
+        .mobile-table {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            width: 80%;
+            height: 20%;
+            background-color: #fffffff0;
+            border-radius: 15px;
+            text-shadow: 1px 1px #6361613b;
+        }
+
+        .on-mobile h3 {
+            text-align: center;
+            color: #D58FDF;
+        }
+
+        .no-mobile {
+            height: 3em;
+        }
+    }
+
+
+</style>
